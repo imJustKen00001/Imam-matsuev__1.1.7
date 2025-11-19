60ace5f4f0efdf6c45eafcef5d68bff2c44dca56
imJustKen00001 <justken00001@gmail.com>
2025-11-20 02:58:43 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+import { createElement } from 'react';
+
+const title = createElement(
+  'h1',
+  { className: 'title' },
+  'you forgot a thousand things every day make sure this is one of them'
+);
+
+createRoot(document.getElementById('root')).render(<>{title}</>);
