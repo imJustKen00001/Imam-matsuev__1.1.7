4cb187baa8cf2769938207edeb3e2b2f57bd84b7
imJustKen00001 <justken00001@gmail.com>
2025-11-20 03:00:39 +0300

    fix symbol

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createElement } from 'react';
 const title = createElement(
   'h1',
   { className: 'title' },
-  'you forgot a thousand things every day make sure this is one of them'
+  'you forgot a thousand things every day make sure this is one of them ©'
 );
 
 createRoot(document.getElementById('root')).render(<>{title}</>);
